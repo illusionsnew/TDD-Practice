4203b190de089f8d81177ab1cc16cdfce7469d0a
jithinjohne <jithinje@gmail.com>
2017-01-29 23:06:13 -0600

    Added new projects

---
+++ TDD/AddStringNumbers.Tests/CalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddStringNumbers.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
