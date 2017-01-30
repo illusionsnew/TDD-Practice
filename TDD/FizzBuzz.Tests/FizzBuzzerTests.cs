71bd16ff4fb1d928e79a7d67a3033b5243540877
jithinjohne <jithinje@gmail.com>
2017-01-29 23:01:46 -0600

    Added unit test project

---
+++ TDD/FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FizzBuzzerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
